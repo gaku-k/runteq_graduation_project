e5b35bda84e858347d408486fa03e49f755f9a88
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2025-10-02 15:26:51 +0900

    newページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   private
 
   def post_params
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,36 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import $ from "jquery";
+import "jquery-raty"; 
+
+// 評価ウィジェットを初期化する関数
+const initializeRaty = () => {
+  const options = {
+    number: 5,
+    starType: 'i',
+    starOff: 'far fa-star',
+    starOn: 'fas fa-star',
+    starHalf: 'fas fa-star-half-alt',
+
+    click: function(score, event) {
+      $(this).siblings('input[type="hidden"]').val(score);
+    },
+
+    score: function() {
+      return $(this).siblings('input[type="hidden"]').val();
+    }
+  };
+
+  // 全てのレーティング用divに対してratyを初期化
+  $('#aroma-rating').raty(options);
+  $('#taste-rating').raty(options);
+  $('#price-rating').raty(options);
+};
+
+
+// 1. DOMが完全に準備されたときに実行
+$(function() {
+    initializeRaty();
+});
+
+// 2. Turboがページ遷移・復元したときに実行
+$(document).on("turbo:load", initializeRaty);
