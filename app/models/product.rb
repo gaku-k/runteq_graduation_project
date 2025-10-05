ca2a58d368a37ad2ceb677b84107cdb6f38d81bc
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2025-10-05 16:57:36 +0900

    スタイルなしサンプル画像表示ロジック

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   has_many :product_olive_varieties, dependent: :destroy
   has_many :olive_varieties, through: :product_olive_varieties
   has_many :posts
+  has_many_attached :images
 end
