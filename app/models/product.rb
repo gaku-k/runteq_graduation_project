d20c3348bd4c99b28d6301b6de6e81bb69f677d8
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2025-10-09 20:31:54 +0900

    モデル動作微修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,8 @@ class Product < ApplicationRecord
   has_many :posts
   has_many_attached :images
 
-  # 直訳:オリーブ品種に対してネストされた属性を許可する。右は関連付け削除のためのオブション
+  # 直訳:オリーブ品種に対してネストされた属性を許可する。
   # Productの新規作成や更新時にProductのインスタンスを操作する際、関連づけられた:olive_varieties の属性も同時に受け付けて保存する機能
-  accepts_nested_attributes_for :olive_varieties, allow_destroy: true
+  # allow_destroy: falseはネストされたフォームを通じて削除要求が行われることを拒否する。ユーザーはOliveVariety レコードの削除ができない前提
+  accepts_nested_attributes_for :olive_varieties, allow_destroy: false
 end
