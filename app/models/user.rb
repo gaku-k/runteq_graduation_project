8253869419679f77a98e1037f0f949226eafa563
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2025-10-14 12:43:46 +0900

    プルリクエラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,5 +39,5 @@ class User < ApplicationRecord
   # ユーザーが削除されたら、その投稿も削除
   has_many :posts, dependent: :destroy
 
-  enum role: { general: 0, admin: 1 }, _default: :general
+  enum role: { general: 0, admin: 1 }
 end
