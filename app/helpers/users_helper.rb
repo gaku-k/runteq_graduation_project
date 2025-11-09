528b54d8edf26affb821f8888276396ca217822d
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2025-11-09 13:29:58 +0900

    post毎にuser-product間の評価値が繰り返されるよう修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
     name, domain = email.split("@")
     # ("*" * (name.lengh - 1))　："*" を (name.lengh - 1)回繰り返す
     masked_name = name[0] + ("*" * (name.size - 1))
-    "#{masked_name}@#{domain}"
+    masked_domain = "*" * domain.size
+    "#{masked_name}@#{masked_domain}"
   end
 end
