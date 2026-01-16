c092f25cfcbf047ae19eaad7cefa8df8dab19580
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2026-01-16 21:17:05 +0900

    テストコントローラー修正

--- test/controllers/admin/product_drafts_controller_test.rb
+++ test/controllers/admin/product_drafts_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class Admin::ProductDraftsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:admin)
+  end
+
   test "should get index" do
-    get admin_product_drafts_index_url
+    get admin_product_drafts_path
     assert_response :success
   end
 
   test "should get show" do
-    get admin_product_drafts_show_url
+    get admin_product_draft_path(draft)
     assert_response :success
   end
 end
