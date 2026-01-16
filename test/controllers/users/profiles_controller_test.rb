e9364a7cb7a68b9ff1e32f8ba66698aecfdfc364
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2026-01-17 08:21:23 +0900

    テストコントローラー修正2

--- test/controllers/users/profiles_controller_test.rb
+++ test/controllers/users/profiles_controller_test.rb
@@ -1,13 +1,24 @@
 require "test_helper"
 
 class Users::ProfilesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get edit" do
-    get users_profiles_edit_url
+    get edit_users_profile_path
     assert_response :success
   end
 
   test "should get update" do
-    get users_profiles_update_url
-    assert_response :success
+    patch users_profile_path, params: {
+      user: {
+        name: "updated name"
+      }
+    }
+
+    assert_response :redirect
   end
 end
