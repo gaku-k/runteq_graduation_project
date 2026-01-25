032b6a3fa13b0bbb3294243b8381f562beab8b09
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2026-01-25 13:13:01 +0900

    ファイル選択時に画像圧縮処理実装

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,7 @@ pin "jquery-raty", to: "jquery.raty.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# app/javascript/custom/test.js を作った場合、app/javascript/application.js に import "custom/test" と書く
+pin_all_from "app/javascript/custom", under: "custom"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "browser-image-compression" # @2.0.2
