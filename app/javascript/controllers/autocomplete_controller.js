7b25d1799718b6d692d882c45f4915c6ddc5a6a3
Kojima Gaku <gaku@kojimanoMacBook-Air.local>
2026-01-31 09:55:16 +0900

    入力でフォーム自動送信

---
+++ app/javascript/controllers/autocomplete_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="autocomplete"
+export default class extends Controller {
+  // html側でdata-autocomplete-target="input".JS側で static targets = ["input"] と定義すれば、this.inputTarget で操作することになる。イベントではなく場所
+  static targets = ["input"]
+
+  connect() {
+    console.log("オートコンプリート準備完了！")
+  }
+
+  search() {
+    // 1文字打つたびに前の予約を消すことで高速タイピング中はサーバー通信を削減
+    // clearTimeoutはjs標準の「予約キャンセルボタン」。()内の引数が存在すれば(予約IDがあれば)実行。
+    clearTimeout(this.timeout)
+    // this.timeoutは0.3秒後にフォームを送信する予約。戻り値として識別IDを返す。
+    this.timeout = setTimeout(() => {
+      // this.element:コントローラーが貼り付けられているhtml要素そのもの
+      // 「あ」と入力したら0.3秒後に文字通り「あ」でフォームを送信する
+      this.element.requestSubmit()
+    }, 300)
+  }
+}
+// このコントローラーだけの処理ではフォームを送信のたびにリロードを挟む。
